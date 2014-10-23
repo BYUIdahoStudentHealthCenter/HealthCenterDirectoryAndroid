d736cd92e79a9c71855df22e1bd176b613ec0b24
Jakob <Jakobhartman@hotmail.com>
2014-10-23 15:00:12 -0600

    Added Active android to the project, created a table for center Directory.

--- app/src/main/java/com/example/jakobhartman/healthcenterdirectory/settings.java
+++ app/src/main/java/com/example/jakobhartman/healthcenterdirectory/settings.java
@@ -5,6 +5,13 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+import localDatabase.DepartmentContact;
+
 
 public class settings extends Activity {
 
@@ -12,6 +19,27 @@ public class settings extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        Firebase.setAndroidContext(this); // initialize firebase
+        Firebase ref = new Firebase("https://boiling-fire-7455.firebaseio.com/");
+                 ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                     @Override
+                     public void onDataChange(DataSnapshot dataSnapshot) {
+                         for(DataSnapshot data : dataSnapshot.getChildren()){
+                             for (DataSnapshot contact : data.getChildren()){
+                                 DepartmentContact newContact = new DepartmentContact();
+                                 newContact.department = data.getName();
+                                 newContact.name = contact.child("Name").getValue().toString();
+                                 newContact.number = contact.child("Number").getValue().toString();
+                                 newContact.save();
+                             }
+                         }
+                     }
+
+                     @Override
+                     public void onCancelled(FirebaseError firebaseError) {
+
+                     }
+                 });
     }
 
 
---
+++ app/src/main/java/localDatabase/HealthcenterModel.java
@@ -0,0 +1,11 @@
+package localDatabase;
+
+import com.activeandroid.Model;
+
+/**
+ * Created by jakobhartman on 10/23/14.
+ */
+public class HealthcenterModel extends Model {
+    public  HealthcenterModel(){super();}
+
+}
